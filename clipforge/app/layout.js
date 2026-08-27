453f2bde5ca959a6bf4b8c9dd88187f330a32ab4
Shailesh Zala <shaileshzala21@gmail.com>
2026-08-27 12:13:48 +0530

    Add files via upload

---
+++ clipforge/app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"ClipForge | Create Once. Repurpose Everywhere.",description:"Free creator tools for repurposing content, generating hooks and writing social media captions."};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
