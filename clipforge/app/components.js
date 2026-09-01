0c54323f04964166512d0f39a20d6d613336c6fc
Shailesh Zala <shaileshzala21@gmail.com>
2026-09-01 14:17:19 +0530

    Update components.js

--- clipforge/app/components.js
+++ clipforge/app/components.js
@@ -1,44 +1,21 @@
 export function Header() {
   return (
-    <header className="site-nav">
-      <div className="wrap site-nav-inner">
+    <header className="site-header">
+      <div className="header-inner">
 
-        <a className="logo" href="/">
+        <a className="site-logo" href="/">
           CLIP<span>FORGE</span>
         </a>
 
-        {/* Desktop Navigation */}
-        <nav className="desktop-nav">
+        <nav className="site-nav">
           <a href="/tools">Tools</a>
           <a href="/content-repurposer">Repurpose</a>
           <a href="#resources">Resources</a>
         </nav>
 
-        <a className="desktop-create-btn" href="/content-repurposer">
+        <a className="header-cta" href="/content-repurposer">
           Start Creating Free
         </a>
-
-        {/* Mobile Menu */}
-        <details className="mobile-menu">
-          <summary aria-label="Open navigation menu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </summary>
-
-          <div className="mobile-menu-panel">
-            <a href="/tools">Tools</a>
-            <a href="/content-repurposer">Repurpose</a>
-            <a href="#resources">Resources</a>
-
-            <a
-              className="mobile-create-btn"
-              href="/content-repurposer"
-            >
-              ✨ Start Creating Free
-            </a>
-          </div>
-        </details>
 
       </div>
     </header>
